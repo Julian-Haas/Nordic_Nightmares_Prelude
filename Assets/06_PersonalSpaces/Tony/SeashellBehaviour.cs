2a071ab5934b58cdffed6db9b7e72fc4f7f1819d
Tony <meis.laura92@googlemail.com>
2024-05-24 14:59:02 +0200

    Seashell_Behaviour_Update
    Animation-Delay

--- Assets/06_PersonalSpaces/Tony/SeashellBehaviour.cs
+++ Assets/06_PersonalSpaces/Tony/SeashellBehaviour.cs
@@ -1,6 +1,6 @@
 //Responsible for this script: Tony Meis
 using System.Collections.Generic;
-//using System.Drawing;
+using System.Collections;
 using UnityEditor;
 using UnityEngine;
 
@@ -15,8 +15,11 @@ public class SeashellBehaviour : MonoBehaviour
     public float MaximumSeashellSize = 2.0f;
     [Range(0.1f,10f)]
     public float RadiusOfArea = 1.0f;
+    [Range(0f, 10f)]
+    public float ReactionArea = 2.0f;
     public bool RandomizeSeashells = true;
 
+    SphereCollider _reactionArea;
     [SerializeField] GameObject seashell;
     [SerializeField] GameObject EMPTY;
 
@@ -28,6 +31,8 @@ public class SeashellBehaviour : MonoBehaviour
         { 
             ResetSeashells();
         }
+        _reactionArea = transform.GetComponent<SphereCollider>();
+        _reactionArea.radius = RadiusOfArea + ReactionArea;
     }
 
     void ResetSeashells()
@@ -37,7 +42,7 @@ public class SeashellBehaviour : MonoBehaviour
 
         for (int i = 0; i < NumberOfSeashells; i++)
         {
-            GameObject newShell = Instantiate(EMPTY);
+            GameObject newShell = Instantiate(seashell);
             RandomizeSeashellTransforms(newShell);
             _seashells.Add(newShell);
         }
@@ -47,17 +52,23 @@ public class SeashellBehaviour : MonoBehaviour
 
     void ClearSeashellList()
     {
-        if ( _seashells.Count >0 )
+        //if ( _seashells.Count > 0 )
+        //{
+        //    foreach (GameObject obj in _seashells)
+        //    {
+        //        if (obj != null)
+        //        {
+        //            DestroyImmediate(obj);
+        //        }
+        //    }
+        //    _seashells.Clear();
+        //}
+
+        while(_seashells.Count > 0)
         {
-            foreach (GameObject obj in _seashells)
-            {
-                if (obj != null)
-                {
-                    DestroyImmediate(obj);
-                }
-            }
-            _seashells.Clear();
+            DestroyImmediate(_seashells[0]);
         }
+        _seashells.Clear();
     }
 
     void RandomizeSeashellTransforms(GameObject obj)
@@ -69,7 +80,7 @@ public class SeashellBehaviour : MonoBehaviour
 
         obj.transform.position = randPos;
         obj.transform.rotation = randomRotation;
-        obj.transform.localScale = new Vector3(1.0f,1.0f,1.0f) * randomScale;
+        obj.transform.localScale = new Vector3(1.0f,1.0f,1.0f) * randomScale * 0.5f;
 
         int loopCount = 0;
         while (loopCount < 25 && !IsPositionValid(obj))
@@ -102,10 +113,49 @@ public class SeashellBehaviour : MonoBehaviour
         return true;
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            foreach(var v in _seashells)
+            {
+                Debug.Log("Trigger Enter Player");
+                StartCoroutine(DelayAnimation(v, "Armature|Close_Mouth"));                
+                //v.GetComponent<Animator>().Play("Armature|Close_Mouth");
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            foreach (var v in _seashells)
+            {
+                StartCoroutine(DelayAnimation(v, "Armature|Open_Mouth"));
+                //v.GetComponent<Animator>().Play("Armature|Open_Mouth");
+            }
+        }
+    }
+
+    IEnumerator DelayAnimation(GameObject shell, string animation)
+    {
+        Debug.Log("Enter Delay " + animation);
+        float tmp = Random.Range(0f, 1f);
+
+        yield return new WaitForSeconds(tmp);
+
+        shell.GetComponent<Animator>().Play(animation);
+    }
+
     void OnDrawGizmos()
     {
         Handles.color = Color.red;
         Handles.DrawWireDisc(transform.position, transform.up.normalized, RadiusOfArea);
+
+        Handles.color = Color.green;
+        Handles.DrawWireDisc(transform.position, transform.up.normalized, RadiusOfArea + ReactionArea);
+
 
         if (RandomizeSeashells)
         {
@@ -113,18 +163,18 @@ public class SeashellBehaviour : MonoBehaviour
 
         }
 
-        if (_seashells.Count > 0)
-        {
-            foreach(var v in _seashells)
-            {
-                if (v)
-                {
-                    Gizmos.color = Color.white;
-                    Matrix4x4 cubeTransform = Matrix4x4.TRS(v.transform.position, v.transform.rotation, v.transform.localScale);
-                    Gizmos.matrix = cubeTransform;
-                    Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
-                }
-            }
-        }
+        //if (_seashells.Count > 0)
+        //{
+        //    foreach(var v in _seashells)
+        //    {
+        //        if (v)
+        //        {
+        //            Gizmos.color = Color.white;
+        //            Matrix4x4 cubeTransform = Matrix4x4.TRS(v.transform.position, v.transform.rotation, v.transform.localScale);
+        //            Gizmos.matrix = cubeTransform;
+        //            Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+        //        }
+        //    }
+        //}
     }
 }
