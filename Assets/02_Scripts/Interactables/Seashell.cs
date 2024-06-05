2259840ab5c5ed6370504daa7519ce4192825e38
Julian-Haas <julian.haas@web.de>
2024-06-05 20:26:00 +0200

    Created Seashell

---
+++ Assets/02_Scripts/Interactables/Seashell.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seashell : Interactable
+{
+    public override bool Interact(bool started) {
+        return true;
+    }
+}
