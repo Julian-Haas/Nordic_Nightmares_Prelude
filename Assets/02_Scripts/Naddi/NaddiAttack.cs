caf7017e24e52ca23ac130169cd5809b1bdce4d4
NilsMac <nils.henningsen@freenet.de>
2024-06-07 14:00:18 +0200

    added offset to Naddi StopDistance to prevent Naddi from standing on one place.

--- Assets/02_Scripts/Naddi/NaddiAttack.cs
+++ Assets/02_Scripts/Naddi/NaddiAttack.cs
@@ -13,18 +13,20 @@ public class NaddiAttack : MonoBehaviour
         _naddiEye = GetComponent<NaddiViewField>();
         _naddiStateMachiene = GetComponent<NaddiStateMaschine>();
     }
+    private float offset = 0.3f; 
     public void ChasePlayer(Transform playerPos)
     {
         if (_naddi.CanChasePlayer)
         {
+
             float sqrMagnitude = (playerPos.position - this.transform.position).sqrMagnitude;
-            if (_naddiEye.isInsideCone() && sqrMagnitude <= Mathf.Pow(_naddi.Agent.stoppingDistance, 2) && !_naddi.PlayerInSafeZone)
+            if (_naddiEye.isInsideCone() && sqrMagnitude <= Mathf.Pow(_naddi.Agent.stoppingDistance + offset, 2) && !_naddi.PlayerInSafeZone)
             {
                 _naddi.ChasePlayer = true;
                 _naddi.Agent.isStopped = true;
                 _naddiStateMachiene.AttackPlayer();
             }
-            else if (((_naddiEye.isInsideCone() && sqrMagnitude > Mathf.Pow(_naddi.Agent.stoppingDistance, 2)) || (sqrMagnitude < Mathf.Pow(_naddi.Agent.stoppingDistance * 5, 2) && sqrMagnitude > Mathf.Pow(_naddi.Agent.stoppingDistance, 2))) && !_naddi.PlayerInSafeZone)
+            else if (((_naddiEye.isInsideCone() && sqrMagnitude > Mathf.Pow(_naddi.Agent.stoppingDistance + offset, 2)) || (sqrMagnitude < Mathf.Pow(_naddi.Agent.stoppingDistance * 5, 2) && sqrMagnitude > Mathf.Pow(_naddi.Agent.stoppingDistance, 2))) && !_naddi.PlayerInSafeZone)
             {
                 _naddi.ChasePlayer = true;
                 if (_naddi.Agent.isPathStale || _naddi.Agent.pathStatus == NavMeshPathStatus.PathInvalid)
@@ -34,7 +36,7 @@ public class NaddiAttack : MonoBehaviour
                 }
                 _naddi.Agent.SetDestination(playerPos.position);
             }
-            else if (!_naddiEye.isInsideCone() && sqrMagnitude > Mathf.Pow(_naddi.Agent.stoppingDistance * 5, 2) && !_naddi.PlayerInSafeZone)
+            else if (!_naddiEye.isInsideCone() && sqrMagnitude > Mathf.Pow((_naddi.Agent.stoppingDistance+ offset) * 5, 2) && !_naddi.PlayerInSafeZone)
             {
                 _naddi.ChasePlayer = false;
                 _naddiStateMachiene.LostPlayer();
